a2780aa7bef591dc761936787cbc6e6801c81e58
baber-sos <baber.khalid@rutgers.edu>
2019-04-11 23:12:29 -0400

    somethings

---
+++ get_images.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2
+import os
+
+# vid_dir = './example_set/';
+vid_dir = '/home/baber/YouTubeClips/';
+vid_file = open('input');
+dest_dir = './images/train/';
+image_size = (224, 224);
+
+if __name__ == '__main__':
+    if not os.path.exists(dest_dir):
+        os.mkdir(dest_dir);
+    for file in vid_file:
+    # for file in os.listdir(vid_dir):
+        # print(file);
+        file = file.strip();
+        vid_file = vid_dir + file;
+        # print(vid_file);
+        vidcap = cv2.VideoCapture(vid_file);
+        success, image = vidcap.read();
+        print(vid_file)
+        print(success);
+        count = 1;
+        video_path = dest_dir + file + '/';
+        if not os.path.exists(video_path):
+            os.mkdir(video_path);
+        while success:
+            image = cv2.resize(image, image_size);
+            cv2.imwrite(video_path + "%d.jpg" % count, image);
+            success, image = vidcap.read();
+            count += 1;
